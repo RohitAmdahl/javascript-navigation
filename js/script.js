544249619d8bbdc4c90dee063967a017b8ebb012
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-13 01:51:40 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@
   const openMenu = function() {
   const burger = document.querySelector(".burger")
   const nav = document.querySelector(".links")
-
+ 
   burger.addEventListener("click", function () {
     nav.classList.toggle("nav-active")
+    
+ 
   });
 
+  burger.classList.toggle("toggle")
 }
 
 openMenu();
